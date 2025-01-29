01812599a9436d44da2000e817ca33cc2f0febba
araven0845 <aravendran05@gmail.com>
2025-01-28 17:04:18 -0800

    initial commit us states guessing game

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import turtle
 import pandas
+
 screen = turtle.Screen()
-screen.title("U.S. 50 States Game")
+screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
-guesses = []
+guessed_states = []
 
-
-while len(guesses) < 50:
-    ans_state = screen.textinput(title=f"{len(guesses)}/50",
-                                 prompt="What's another states name?").title()
-    if ans_state == "Exit":
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
         missing_states = []
         for state in all_states:
-            if state not in guesses:
+            if state not in guessed_states:
                 missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("States_to_learn.csv")
+        new_data.to_csv("states_to_learn.csv")
         break
-    if ans_state in all_states:
-        guesses.append(ans_state)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
-        t.pu()
         t.hideturtle()
-        state_data = data[data.state == ans_state]
+        t.penup()
+        state_data = data[data.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
-        t.write(ans_state)
-
-turtle.mainloop()
+        t.write(answer_state)
